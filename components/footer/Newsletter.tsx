2fc249c400672351f3062d48704efb6f9d7a7ce8
Gustavo Rocha <gustavo@onserp.com.br>
2023-11-21 19:21:48 -0300

    add mailchimp newsletter

--- components/footer/Newsletter.tsx
+++ components/footer/Newsletter.tsx
@@ -27,21 +27,6 @@ function Newsletter(
   const { tiled = false } = layout;
   const loading = useSignal(false);
 
-  const handleSubmit: JSX.GenericEventHandler<HTMLFormElement> = async (e) => {
-    e.preventDefault();
-
-    try {
-      loading.value = true;
-
-      const email =
-        (e.currentTarget.elements.namedItem("email") as RadioNodeList)?.value;
-
-      await invoke.vtex.actions.newsletter.subscribe({ email });
-    } finally {
-      loading.value = false;
-    }
-  };
-
   return (
     <div
       class={`flex ${
@@ -50,40 +35,84 @@ function Newsletter(
           : "flex-col gap-4"
       }`}
     >
-      <div class="flex flex-col gap-4">
-        {content?.title && (
-          <h3 class={tiled ? "text-2xl lg:text-3xl" : "text-lg"}>
-            {content?.title}
-          </h3>
-        )}
-        {content?.description && <div>{content?.description}</div>}
-      </div>
-      <div class="flex flex-col gap-4">
-        <form
-          class="form-control"
-          onSubmit={handleSubmit}
-        >
-          <div class="flex flex-wrap gap-3">
-            <input
-              name="email"
-              class="flex-auto md:flex-none input input-bordered md:w-80 text-base-content"
-              placeholder={content?.form?.placeholder || "Digite seu email"}
-            />
-            <button
-              type="submit"
-              class="btn disabled:loading"
-              disabled={loading}
-            >
-              {content?.form?.buttonText || "Inscrever"}
-            </button>
-          </div>
-        </form>
-        {content?.form?.helpText && (
-          <div
-            class="text-sm"
-            dangerouslySetInnerHTML={{ __html: content?.form?.helpText }}
-          />
-        )}
+      <div id="mc_embed_shell">
+        <div id="mc_embed_signup">
+          <form
+            action="https://eletrofrigor.us14.list-manage.com/subscribe/post?u=b2ed99d90f32a9514f549fc33&amp;id=9d7b138f6e&amp;f_id=00a1a7e0f0"
+            method="post"
+            id="mc-embedded-subscribe-form"
+            name="mc-embedded-subscribe-form"
+            class="validate"
+            target="_blank"
+          >
+            <div id="mc_embed_signup_scroll" class="mb-4">
+              <span class="font-medium text-lg">
+                Fique por dentro das novidades
+              </span>
+            </div>
+
+            <div class="mc-field-group mb-4">
+              <label for="mce-FNAME" class="sr-only">
+                Nome <span class="asterisk"></span>
+              </label>
+              <input
+                type="text"
+                name="FNAME"
+                class="required flex-auto md:flex-none input input-bordered md:w-80 text-base-content"
+                id="mce-FNAME"
+                placeholder="Nome"
+                required
+                value=""
+              />
+            </div>
+
+            <div class="mc-field-group mb-4">
+              <label for="mce-EMAIL" class="sr-only">
+                Endereço de e-mail <span class="asterisk"></span>
+              </label>
+              <input
+                type="email"
+                name="EMAIL"
+                class="required flex-auto md:flex-none input input-bordered md:w-80 text-base-content"
+                id="mce-EMAIL"
+                required
+                placeholder="Digite seu E-mail"
+                value=""
+              />
+            </div>
+
+            <div aria-hidden="true" style="position: absolute; left: -5000px;">
+              {
+                /* real people should not fill this in and expect good things - do
+              not remove this or risk form bot signups */
+              }
+              <input
+                type="text"
+                name="b_b2ed99d90f32a9514f549fc33_9d7b138f6e"
+                tabindex={-1}
+                value=""
+              />
+            </div>
+
+            <div class="optionalParent">
+              <div class="flex justify-end">
+                <input
+                  type="submit"
+                  name="subscribe"
+                  id="mc-embedded-subscribe"
+                  class="btn"
+                  value="Subscribe"
+                />
+              </div>
+            </div>
+          </form>
+
+          <script
+            type="text/javascript"
+            src="//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js"
+          >
+          </script>
+        </div>
       </div>
     </div>
   );
